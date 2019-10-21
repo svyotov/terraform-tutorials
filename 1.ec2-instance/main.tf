f40c3295daa845e1f22b8a82868fa72a3016b898
Pavan Aleti <pavan.hpsa@gmail.com>
2019-10-21 14:34:35 +0100

    ec2

---
+++ 1.ec2-instance/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-098bb5d92c8886ca1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "class-instnace"
+  }
+}
