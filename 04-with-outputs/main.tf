7cfefb2d2e1ff51db4f79ef6f0113b494180dea0
Stanislav Yotov <29090864+svyotov@users.noreply.github.com>
2022-01-27 15:21:13 +0000

    renames

--- 04-with-outputs/main.tf
+++ 04-with-outputs/main.tf
@@ -2,7 +2,5 @@ resource "aws_instance" "ec2" {
   ami           = lookup(var.amis, var.region)
   instance_type = var.instance_type
 
-  tags = {
-    Name = var.tags
-  }
+  tags = {Name  = var.tags}
 }
